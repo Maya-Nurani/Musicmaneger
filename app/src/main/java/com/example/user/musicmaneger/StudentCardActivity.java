c00d28e05e3f3a0225bfdbaef9cc9139a57fd063
MayaNurani <maya.nurani@theculturetrip.com>
2018-12-09 20:32:31 +0200

    Add action for moving from student card page to main activity

--- app/src/main/java/com/example/user/musicmaneger/StudentCardActivity.java
+++ app/src/main/java/com/example/user/musicmaneger/StudentCardActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.musicmaneger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.logging.Logger;
@@ -15,6 +17,14 @@ public class StudentCardActivity extends Activity {
         setContentView(R.layout.activity_student_card);
 
         Button backButton = findViewById(R.id.back_Button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                log.info("Trigger to student card");
+                Intent intent = new Intent(StudentCardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
