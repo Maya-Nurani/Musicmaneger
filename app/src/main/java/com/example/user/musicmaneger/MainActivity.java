70117daf2a131670c20542d0f422eea5d4da898a
MayaNurani <maya.nurani@theculturetrip.com>
2018-12-09 20:22:23 +0200

    1. Add button for connect between the the main page and the student card page, and connect between the page by using intent 2. Change the layout of student card page to be mix of LinearLayout and RelativeLayout.

--- app/src/main/java/com/example/user/musicmaneger/MainActivity.java
+++ app/src/main/java/com/example/user/musicmaneger/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.user.musicmaneger;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +10,7 @@ import android.widget.Toast;
 import java.util.logging.Logger;
 
 public class MainActivity extends Activity {
-    public Button button2;
+
     public Logger log = Logger.getLogger("log text?");
 
     @Override
@@ -18,17 +18,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // NOTE: About to wire up the buttons
-        button2 = findViewById(R.id.seconed_button);
+        Button button2 = findViewById(R.id.seconed_button);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 log.info("toast display after click on button");
-                Toast.makeText(getApplicationContext(),"I did something!" , Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "I did something!", Toast.LENGTH_LONG).show();
             }
         });
-    }
+
+        Button card = findViewById(R.id.student_card_trigger);
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                log.info("Trigger to student card");
+                Intent intent = new Intent(MainActivity.this, StudentCardActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 /*    protected void onStart (Bundle savedInstanceState){
 
     }*/
+    }
 }
--- app/src/main/java/com/example/user/musicmaneger/StudentCardActivity.java
+++ app/src/main/java/com/example/user/musicmaneger/StudentCardActivity.java
@@ -2,6 +2,7 @@ package com.example.user.musicmaneger;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.Button;
 
 import java.util.logging.Logger;
 
@@ -12,6 +13,9 @@ public class StudentCardActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_card);
+
+        Button backButton = findViewById(R.id.back_Button);
+
     }
 
 }
